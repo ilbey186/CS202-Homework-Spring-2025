0f015c9ed35b3106f30be86bd07be051ac4aa21a
ilbey186 <milbeyangur188@gmail.com>
2025-04-19 16:10:43 +0300

    Update Drone.cpp

--- Homework2/Drone.cpp
+++ Homework2/Drone.cpp
@@ -1,1 +1,20 @@
+#include "Drone.h"
 
+Drone::Drone() : id(0), batteryLife(0.0), speed(0.0), cooldownTicks(0), x(0.0), y(0.0) {}
+
+Drone::Drone(int id, double batteryLife, double speed, double x, double y)
+    : id(id), batteryLife(batteryLife), speed(speed), cooldownTicks(0), x(x), y(y) {}
+
+bool Drone::compare(const Drone& d1, const Drone& d2) 
+{
+    if (d1.batteryLife == d2.batteryLife) 
+    {
+        return d1.id < d2.id; // Lower ID gets priority if battery is the same
+    }
+    return d1.batteryLife > d2.batteryLife;
+}
+
+void Drone::resetBattery() 
+{
+    batteryLife = 300.0; // Reset to full battery after cooldown
+}
